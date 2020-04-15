e91eb2fc9c18d0a8ad09399c183979f40e55556a
Jensen Daniel <jadd3791@gmail.com>
2020-04-15 14:36:26 +0200

    created notification, trip, and ketchup models and controllers w/o method

---
+++ app/controllers/ketchups_controller.rb
@@ -0,0 +1,2 @@
+class KetchupsController < ApplicationController
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :trips, dependent: :destroy
+  has_many :notifications, dependent: :destroy
+  has_many :ketchups, through: :trips
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
 end
