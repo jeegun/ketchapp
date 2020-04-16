825058148213bc8ea2ba249bb441c95aec36ab0f
Sang Soo Ra <nadang82@gmail.com>
2020-04-16 12:46:15 +0200

    trip creation and trip show function done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_101243) do
+ActiveRecord::Schema.define(version: 2020_04_16_103636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2020_04_16_101243) do
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -80,4 +82,5 @@ ActiveRecord::Schema.define(version: 2020_04_16_101243) do
   add_foreign_key "notifications", "ketchups"
   add_foreign_key "notifications", "trips"
   add_foreign_key "notifications", "users"
+  add_foreign_key "trips", "users"
 end
