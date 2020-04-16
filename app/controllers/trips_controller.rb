17776c5bd455a263db7fc58586ebdda258f8f8ff
Sang Soo Ra <nadang82@gmail.com>
2020-04-16 20:09:49 +0200

    chaged h1 style, added function to capitalize user input for location in trip, added status to trip, changed button for ketchup form and added title to trip show page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,6 +5,17 @@ class TripsController < ApplicationController
     @trip = Trip.all
   end
 
+  def show
+    @friends = User.where(["home_city = ?", @trip.location])
+    @ketchup = Ketchup.new
+    @start_year = @trip.start_date.strftime('%Y')
+    @start_month = @trip.start_date.strftime('%b')
+    @start_day = @trip.start_date.strftime('%d')
+    @end_year = @trip.end_date.strftime('%Y')
+    @end_month = @trip.end_date.strftime('%b')
+    @end_day = @trip.end_date.strftime('%d')
+  end
+
   def new
     @trip = Trip.new
   end
@@ -12,16 +23,13 @@ class TripsController < ApplicationController
   def create
     @trip = Trip.new(trip_params)
     @trip.user = current_user
+    @trip.location = (@trip.location.split(' ').map { |e| e.capitalize! }).join(' ')
+    @trip.status = "saved"
     if @trip.save
       redirect_to trip_path(@trip)
     else
       render 'pages/home'
     end
-  end
-
-  def show
-    @friends = User.where(["home_city = ?", @trip.location])
-    @ketchup = Ketchup.new
   end
 
   def edit
