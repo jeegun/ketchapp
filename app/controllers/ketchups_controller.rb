40c567ddaa1b7d7fc05d2ab770ad7517d5161cc4
Sang Soo Ra <nadang82@gmail.com>
2020-04-16 20:30:59 +0200

    updated h1 margin and printed out details of ketchup in show page

--- app/controllers/ketchups_controller.rb
+++ app/controllers/ketchups_controller.rb
@@ -2,6 +2,11 @@ class KetchupsController < ApplicationController
   before_action :set_ketchup, only: [:show, :destroy]
 
   def show
+    @year = @ketchup.date.strftime('%Y')
+    @month = @ketchup.date.strftime('%b')
+    @day = @ketchup.date.strftime('%d')
+    @hour = @ketchup.start_time.strftime('%H')
+    @minute = @ketchup.start_time.strftime('%M')
   end
 
   def create
