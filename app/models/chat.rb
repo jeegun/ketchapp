ad8c13b99d7dc4b2cd4edb5e32e79e6033b91c6a
Sang Soo Ra <nadang82@gmail.com>
2020-04-21 16:11:53 +0200

    commented out chat functions not in use for now

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -6,19 +6,19 @@ class Chat < ApplicationRecord
   validates :sender_id, uniqueness: { scope: :recipient_id }
 
 # returns a chat between two requested users
-  scope :between, -> (sender_id, recipient_id) do
-    where(sender_id: sender_id, recipient_id: recipient_id).or(
-      where(sender_id: recipient_id, recipient_id: sender_id)
-    )
-  end
+  # scope :between, -> (sender_id, recipient_id) do
+  #   where(sender_id: sender_id, recipient_id: recipient_id).or(
+  #     where(sender_id: recipient_id, recipient_id: sender_id)
+  #   )
+  # end
 
 # tries to get a chat between two users – and if it’s present, it returns it. If there isn’t any, it creates a new one
-  def self.get(sender_id, recipient_id)
-    chat = between(sender_id, recipient_id).first
-    return chat if chat.present?
+  # def self.get(sender_id, recipient_id)
+  #   chat = between(sender_id, recipient_id).first
+  #   return chat if chat.present?
 
-    create(sender_id: sender_id, recipient_id: recipient_id)
-  end
+  #   create(sender_id: sender_id, recipient_id: recipient_id)
+  # end
 
 # returns an opposed user to the requested user
   def opposed_user(user)
