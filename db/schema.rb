2740a669877caf393b30ec9b86f89dc13093f0f3
Sang Soo Ra <nadang82@gmail.com>
2020-04-21 13:03:38 +0200

    updated seed file

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 2020_04_21_094738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "chats", force: :cascade do |t|
-    t.integer "recipient_id"
-    t.integer "sender_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["recipient_id", "sender_id"], name: "index_chats_on_recipient_id_and_sender_id", unique: true
-  end
-
   create_table "friend_requests", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "receiver_id"
@@ -62,16 +54,6 @@ ActiveRecord::Schema.define(version: 2020_04_21_094738) do
     t.datetime "end_date"
     t.index ["trip_id"], name: "index_ketchups_on_trip_id"
     t.index ["user_id"], name: "index_ketchups_on_user_id"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text "body"
-    t.bigint "user_id"
-    t.bigint "chat_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["chat_id"], name: "index_messages_on_chat_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "notifications", force: :cascade do |t|
@@ -124,8 +106,6 @@ ActiveRecord::Schema.define(version: 2020_04_21_094738) do
 
   add_foreign_key "ketchups", "trips"
   add_foreign_key "ketchups", "users"
-  add_foreign_key "messages", "chats"
-  add_foreign_key "messages", "users"
   add_foreign_key "notifications", "ketchups"
   add_foreign_key "notifications", "trips"
   add_foreign_key "notifications", "users"
