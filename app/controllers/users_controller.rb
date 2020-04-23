21a0640e09d3a1a184ff701c2a1e79dcab1b94fd
Sang Soo Ra <nadang82@gmail.com>
2020-04-23 16:31:07 +0200

    showing the users that matches either email or phone from contact and filtered with friend requests and friendship

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,5 +13,18 @@ class UsersController < ApplicationController
     @sent_requests = FriendRequest.where(["sender_id = ? AND status = ?", current_user.id, "pending"])
     @received_requests = FriendRequest.where(["receiver_id = ? AND status = ?", current_user.id, "pending"])
     @friendship = Friendship.new
+    contacts = current_user.contacts
+    matching_contacts = contacts.map do |contact|
+      if (User.where(["phone_number = ? OR email = ?", contact.phone_number, contact.email]).first != nil)
+        User.where(["phone_number = ? OR email = ?", contact.phone_number, contact.email]).first
+      end
+    end
+    matching_contacts.compact!
+    @requestable_users = matching_contacts.map do |contact|
+      if (!current_user.is_friend?(current_user.id, contact.id) && !FriendRequest.where(["sender_id = ? AND receiver_id = ? AND status = ?", current_user.id, contact.id, "pending"]).any? && !FriendRequest.where(["sender_id = ? AND receiver_id = ? AND status = ?", contact.id, current_user.id, "pending"]).any?)
+        contact
+      end
+    end
+    @requestable_users.compact!
   end
 end
