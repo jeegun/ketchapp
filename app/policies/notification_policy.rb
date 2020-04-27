14eed23ae6a1157a09bb01a76a181b47bb7a5539
Sang Soo Ra <nadang82@gmail.com>
2020-04-27 18:44:29 +0200

    added mark as read in notification policy

--- app/policies/notification_policy.rb
+++ app/policies/notification_policy.rb
@@ -4,4 +4,14 @@ class NotificationPolicy < ApplicationPolicy
       scope.where(recipient: user).unread
     end
   end
+
+  def mark_as_read?
+    user_is_recipient?
+  end
+
+  private
+
+  def user_is_recipient?
+    record.recipient == user
+  end
 end
