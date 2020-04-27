063e9403f22e3363e6cd4b3e964f9d259990ef0f
Sang Soo Ra <nadang82@gmail.com>
2020-04-27 20:03:20 +0200

    removed migration file to remove foreign keys from notification

--- db/migrate/20200426193041_remove_ids_from_notifications.rb
+++
@@ -1,7 +0,0 @@
-class RemoveIdsFromNotifications < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :notifications, :ketchup_id, :string
-    remove_column :notifications, :trip_id, :string
-    remove_column :notifications, :user_id, :string
-  end
-end
