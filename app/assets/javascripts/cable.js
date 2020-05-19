deaa4b9d5d6a3e049fa6fb2462b9565824b4d089
Jee Gun Kim <jeegun@gmail.com>
2020-05-19 18:11:43 +0900

    action cable structure set up

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -2,12 +2,13 @@
 // You can generate new channels where WebSocket features live using the `rails generate channel` command.
 //
 //= require action_cable
+//= require jquery
 //= require_self
 //= require_tree ./channels
 
 (function() {
   this.App || (this.App = {});
 
-  App.cable = ActionCable.createConsumer();
+  App.cable = ActionCable.createConsumer($('meta[name=action-cable-url]').attr('content'));
 
 }).call(this);
