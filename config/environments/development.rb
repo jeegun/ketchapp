2c25bb8b60ed1c1922a86ae135cbd9bf8088e9eb
Sang Soo Ra <nadang82@gmail.com>
2020-06-08 20:51:11 +0200

    action cable configuration for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,9 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.action_cable.mount_path = "/cable"
+  Rails.application.configure do
+    config.action_cable.url = "ws://localhost:3000/cable"
+  end
 
   # Do not eager load code on boot.
   config.eager_load = false
